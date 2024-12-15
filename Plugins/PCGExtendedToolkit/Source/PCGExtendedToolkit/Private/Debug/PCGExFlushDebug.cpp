d57157a2519d0c4d8d4131cd012b30e3118a508d
Tim <nebukam@gmail.com>
2024-12-14 19:27:27 -0500

    Update

---
+++ Plugins/PCGExtendedToolkit/Source/PCGExtendedToolkit/Private/Debug/PCGExFlushDebug.cpp
@@ -0,0 +1,75 @@
+﻿// Copyright 2024 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#include "Debug/PCGExFlushDebug.h"
+
+#include "PCGGraph.h"
+#include "PCGPin.h"
+
+#define LOCTEXT_NAMESPACE "PCGExGraphSettings"
+#define PCGEX_NAMESPACE Debug
+
+#pragma region UPCGSettings interface
+
+TArray<FPCGPinProperties> UPCGExDebugSettings::InputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PCGEX_PIN_ANY(PCGEx::SourcePointsLabel, "In.", Required, {})
+	return PinProperties;
+}
+
+TArray<FPCGPinProperties> UPCGExDebugSettings::OutputPinProperties() const
+{
+	TArray<FPCGPinProperties> PinProperties;
+	PCGEX_PIN_ANY(PCGEx::SourcePointsLabel, "Out.", Required, {})
+	return PinProperties;
+}
+
+FPCGElementPtr UPCGExDebugSettings::CreateElement() const { return MakeShared<FPCGExDebugElement>(); }
+
+#pragma endregion
+
+FPCGContext* FPCGExDebugElement::Initialize(
+	const FPCGDataCollection& InputData,
+	const TWeakObjectPtr<UPCGComponent> SourceComponent,
+	const UPCGNode* Node)
+{
+	FPCGExDebugContext* Context = new FPCGExDebugContext();
+
+	Context->InputData = InputData;
+	Context->SourceComponent = SourceComponent;
+	Context->Node = Node;
+
+	return Context;
+}
+
+bool FPCGExDebugElement::ExecuteInternal(FPCGContext* InContext) const
+{
+	PCGEX_CONTEXT_AND_SETTINGS(Debug)
+
+#if WITH_EDITOR
+
+	if (!Settings->bPCGExDebug)
+	{
+		DisabledPassThroughData(Context);
+		return true;
+	}
+
+	if (Context->bWait)
+	{
+		Context->bWait = false;
+		return false;
+	}
+
+	FlushPersistentDebugLines(PCGEx::GetWorld(Context));
+	FlushDebugStrings(PCGEx::GetWorld(Context));
+
+#endif
+
+	DisabledPassThroughData(Context);
+
+	return true;
+}
+
+#undef LOCTEXT_NAMESPACE
+#undef PCGEX_NAMESPACE
---
+++ Plugins/PCGExtendedToolkit/Source/PCGExtendedToolkit/Public/Debug/PCGExFlushDebug.h
@@ -0,0 +1,61 @@
+﻿// Copyright 2024 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PCGEx.h"
+#include "PCGExPointsProcessor.h"
+
+#include "PCGExFlushDebug.generated.h"
+
+UCLASS(MinimalAPI, BlueprintType, ClassGroup = (Procedural))
+class /*PCGEXTENDEDTOOLKIT_API*/ UPCGExDebugSettings : public UPCGSettings
+{
+	GENERATED_BODY()
+
+	friend class FPCGExDebugElement;
+
+public:
+	//~Begin UPCGSettings
+#if WITH_EDITOR
+	bool bCacheResult = false;
+	PCGEX_NODE_INFOS(FlushDebug, "Flush Debug", "Flush persistent debug lines.");
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Debug; }
+	virtual FLinearColor GetNodeTitleColor() const override { return CustomColor; }
+#endif
+
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings
+
+	/** Debug drawing toggle. Exposed to have more control on debug draw in sub-graph. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Debug, meta=(PCG_Overridable))
+	bool bPCGExDebug = true;
+
+	/** Debug drawing toggle. Exposed to have more control on debug draw in sub-graph. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Debug, meta=(PCG_Overridable))
+	FLinearColor CustomColor = FLinearColor(1.0f, 0.0f, 0.0f, 1.0f);
+};
+
+struct /*PCGEXTENDEDTOOLKIT_API*/ FPCGExDebugContext final : FPCGContext
+{
+	friend class FPCGExDebugElement;
+	bool bWait = true;
+};
+
+class /*PCGEXTENDEDTOOLKIT_API*/ FPCGExDebugElement final : public IPCGElement
+{
+public:
+	virtual FPCGContext* Initialize(const FPCGDataCollection& InputData, TWeakObjectPtr<UPCGComponent> SourceComponent, const UPCGNode* Node) override;
+	virtual bool IsCacheable(const UPCGSettings* InSettings) const override { return false; }
+	virtual bool CanExecuteOnlyOnMainThread(FPCGContext* Context) const override { return true; }
+
+	//virtual void DisabledPassThroughData(FPCGContext* Context) const override;
+
+protected:
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+};
