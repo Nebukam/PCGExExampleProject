a47223f762509647f26a066f01ea41705bc0246d
Tim <nebukam@gmail.com>
2025-01-28 21:59:48 -0500

    Removing plugin source from example project

--- Plugins/PCGExtendedToolkit/Source/PCGExtendedToolkit/Private/PCGExBlueprintHelpers.cpp
+++
@@ -1,7 +0,0 @@
-﻿// Copyright 2025 Timothé Lapetite and contributors
-// Released under the MIT license https://opensource.org/license/MIT/
-
-#include "PCGExBlueprintHelpers.h"
-
-#include "PCGExSubSystem.h"
-#include "Engine/World.h"
--- Plugins/PCGExtendedToolkit/Source/PCGExtendedToolkit/Private/Transform/PCGExTransform.cpp
+++
@@ -1,6 +0,0 @@
-﻿// Copyright 2025 Timothé Lapetite and contributors
-// Released under the MIT license https://opensource.org/license/MIT/
-
-#pragma once
-
-#include "Transform/PCGExTransform.h"
